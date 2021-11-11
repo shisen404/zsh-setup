d79c29af1dacdcfd4bc472d7f968e1f73809b63d
Shivam Singh Sengar <shvmsengars3@gmail.com>
2021-11-11 23:22:14 +0530

    Update extraAliases.zsh

--- extraAliases.zsh
+++ extraAliases.zsh
@@ -1,6 +1,14 @@
+# extraAliases
 fcd(){
   cd "$(find -type d | fzf)"
 }
+
+open(){
+  xdg-open "$(find -type f | fzf)"
+}
+
+psInfo() {ps -p "$1" -o pid,cmd,etime,etimes,uid,gid}
+ptemp() {curl wttr.in/"$1"}
 
 open(){
   xdg-open "$(find -type f | fzf)"
@@ -11,3 +19,8 @@ alias getpath="find -type f | fzf | sed 's/^..//' | tr -d '\n' | xclip -selectio
 #Clipboard
 alias cin="xclip -selection c"
 alias cout="xclip -selection Clipboard -o"
+
+# Create a new directory and enter it
+function mk() {
+  mkdir -p "$@" && cd "$@"
+}
